f6494fe9633028ecbb8118193124627d2717e46a
nomiso2000 <nomiso.study@gmail.com>
2020-08-31 10:43:18 +0300

    new BagPug

--- src/Components/pagination/pagination.js
+++ src/Components/pagination/pagination.js
@@ -7,11 +7,13 @@ import { starterMainPage } from '../mainPage/mainPage.js';
 import { setQuery } from '../header/search/search_api.js';
 import getMovieByQuery from '../../api_services';
 import { createMarkup, getId } from '../mainPage/mainPage.js';
+import { refs } from '../../refs.js';
+import { movieList } from '../movieList/movieList.js';
 
 export const Pagination = {
   code: '',
   size: 0, // pages size
-  page: APIhelpers.page, // selected page
+
   step: 2, // pages before and after current
   context: '',
   resultsQuery: [],
@@ -19,7 +21,7 @@ export const Pagination = {
   // add pages by number (from [s] to [f])
   Add: function (s, f) {
     for (var i = s; i < f; i++) {
-      Pagination.code += '<a>' + i + '</a>';
+      this.code += '<a>' + i + '</a>';
     }
   },
 
@@ -41,20 +43,22 @@ export const Pagination = {
     Pagination.code += '<a>1</a><i>...</i>';
   },
 
-  // change page
-  Click: function () {
-    //  Pagination.page=1
-
-    Pagination.page = +this.innerHTML;
-    Pagination.Start();
-
+  APIandMarkup: function () {
     APIhelpers.page = Pagination.page;
     // APIhelpers.page = Number(event.target.textContent);
     if (Pagination.context === 'mainPage') {
-      starterMainPage();
+      const result = API.getMovies().then(result => {
+        const markup = movieList(result.results);
+
+        refs.container.innerHTML = `<ul class="movies_list">${markup}</ul>`;
+        const movie_list = document.querySelector('.movies_list');
+        movie_list.addEventListener('click', getId);
+      });
     }
 
     if (Pagination.context === 'search_api') {
+      APIhelpers.page = Number(event.target.textContent);
+
       APIhelpers.query = Pagination.resultQuery;
       async function fun() {
         const data = await getMovieByQuery.getMovieByQuery();
@@ -65,8 +69,13 @@ export const Pagination = {
       }
       fun();
     }
+  },
 
-    Pagination.context = '';
+  // change page
+  Click: function () {
+    Pagination.page = +this.innerHTML;
+    Pagination.Start();
+    this.APIandMarkup();
   },
 
   // previous page
@@ -76,25 +85,7 @@ export const Pagination = {
       Pagination.page = 1;
     }
     Pagination.Start();
-    APIhelpers.page = Pagination.page;
-    // APIhelpers.page = Number(event.target.textContent);
-    if (Pagination.context === 'mainPage') {
-      starterMainPage();
-    }
-
-    if (Pagination.context === 'search_api') {
-      APIhelpers.query = Pagination.resultQuery;
-      async function fun() {
-        const data = await getMovieByQuery.getMovieByQuery();
-
-        createMarkup(data.results);
-        const movie_list = document.querySelector('.movies_list');
-        movie_list.addEventListener('click', getId);
-      }
-      fun();
-    }
-
-    // Pagination.context ='';
+    this.APIandMarkup();
   },
 
   // next page
@@ -105,25 +96,7 @@ export const Pagination = {
       Pagination.page = Pagination.size;
     }
     Pagination.Start();
-    APIhelpers.page = Pagination.page;
-    // APIhelpers.page = Number(event.target.textContent);
-    if (Pagination.context === 'mainPage') {
-      starterMainPage();
-    }
-
-    if (Pagination.context === 'search_api') {
-      APIhelpers.query = Pagination.resultQuery;
-      async function fun() {
-        const data = await getMovieByQuery.getMovieByQuery();
-
-        createMarkup(data.results);
-        const movie_list = document.querySelector('.movies_list');
-        movie_list.addEventListener('click', getId);
-      }
-      fun();
-    }
-
-    // Pagination.context ='';
+    this.APIandMarkup();
   },
 
   // binding pages
@@ -143,66 +116,64 @@ export const Pagination = {
   },
 
   // find pagination type
-//   Start: function () {
-//     if (Pagination.size < Pagination.step * 2 + 6) {
-//       Pagination.Add(1, Pagination.size + 1);
-//     } else if (Pagination.page < Pagination.step * 2 + 1) {
-//       if (info.isMobile) {
-//         Pagination.Add(1, Pagination.step * 2 + 2);
-//       } else {
-//         Pagination.Add(1, Pagination.step * 2 + 4);
-//       }
-//       Pagination.Last();
-//     } else if (Pagination.page > Pagination.size - Pagination.step * 2) {
-//       Pagination.First();
-//       Pagination.Add(
-//         Pagination.size - Pagination.step * 2 - 2,
-//         Pagination.size + 1,
-//       );
-//     } else {
-//       Pagination.First();
-//       Pagination.Add(
-//         Pagination.page - Pagination.step,
-//         Pagination.page + Pagination.step + 1,
-//       );
-//       Pagination.Last();
-//     }
-//     Pagination.Finish();
-//   },
+  Start: function () {
+    if (Pagination.size < Pagination.step * 2 + 6) {
+      Pagination.Add(1, Pagination.size + 1);
+    } else if (Pagination.page < Pagination.step * 2 + 1) {
+      if (info.isMobile) {
+        Pagination.Add(1, Pagination.step * 2 + 2);
+      } else {
+        Pagination.Add(1, Pagination.step * 2 + 4);
+      }
+      Pagination.Last();
+    } else if (Pagination.page > Pagination.size - Pagination.step * 2) {
+      Pagination.First();
+      Pagination.Add(
+        Pagination.size - Pagination.step * 2 - 2,
+        Pagination.size + 1,
+      );
+    } else {
+      Pagination.First();
+      Pagination.Add(
+        Pagination.page - Pagination.step,
+        Pagination.page + Pagination.step + 1,
+      );
+      Pagination.Last();
+    }
+    Pagination.Finish();
+  },
 
-//   // binding buttons
-//   Buttons: function (e) {
-//     var nav = e.getElementsByTagName('div');
-//     nav[0].addEventListener('click', Pagination.Prev, false);
-//     nav[1].addEventListener('click', Pagination.Next, false);
-//   },
+  // binding buttons
+  Buttons: function (e) {
+    var nav = e.getElementsByTagName('div');
+    nav[0].addEventListener('click', Pagination.Prev, false);
+    nav[1].addEventListener('click', Pagination.Next, false);
+  },
 
-//   // create skeleton
-//   Create: function (e) {
-//     var html = [
-//       '<div class="arrowLeftWrapper"><img id="al" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTEyLjY2NjYgOEgzLjMzMzI1IiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjEuMzMzMzMiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgo8cGF0aCBkPSJNNy45OTk5MiAxMi42NjY3TDMuMzMzMjUgOC4wMDAwNEw3Ljk5OTkyIDMuMzMzMzciIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMS4zMzMzMyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+Cjwvc3ZnPgo="></></div>', // previous button
-//       '<span class="pages_list"></span>', // pagination container
-//       '<div class="arrowRightWrapper"><img id="ar" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik0zLjMzMzI5IDhIMTIuNjY2NiIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIxLjMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4NCjxwYXRoIGQ9Ik03Ljk5OTk2IDEyLjY2NjdMMTIuNjY2NiA4LjAwMDA0TDcuOTk5OTYgMy4zMzMzNyIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIxLjMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4NCjwvc3ZnPg0K"></></div>', // next button
-//     ];
+  // create skeleton
+  Create: function (e) {
+    var html = [
+      '<div class="arrowLeftWrapper"><img id="al" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTEyLjY2NjYgOEgzLjMzMzI1IiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjEuMzMzMzMiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgo8cGF0aCBkPSJNNy45OTk5MiAxMi42NjY3TDMuMzMzMjUgOC4wMDAwNEw3Ljk5OTkyIDMuMzMzMzciIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMS4zMzMzMyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+Cjwvc3ZnPgo="></></div>', // previous button
+      '<span class="pages_list"></span>', // pagination container
+      '<div class="arrowRightWrapper"><img id="ar" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik0zLjMzMzI5IDhIMTIuNjY2NiIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIxLjMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4NCjxwYXRoIGQ9Ik03Ljk5OTk2IDEyLjY2NjdMMTIuNjY2NiA4LjAwMDA0TDcuOTk5OTYgMy4zMzMzNyIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIxLjMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4NCjwvc3ZnPg0K"></></div>', // next button
+    ];
 
-//     e.innerHTML = html.join('');
-//     Pagination.e = e.getElementsByTagName('span')[0];
-//     Pagination.Buttons(e);
-//   },
+    e.innerHTML = html.join('');
+    Pagination.e = e.getElementsByTagName('span')[0];
+    Pagination.Buttons(e);
+  },
 
-//   Clear: function () {
-//     document.getElementById('pagination').innerHTML = '';
-//   },
+  Clear: function () {
+    document.getElementById('pagination').innerHTML = '';
+  },
 
-//   Init: function (size, context, resultQuery) {
-//     Pagination.Clear();
-//     Pagination.context = context;
-//     Pagination.size = size;
-//     // Pagination.page =1;
-//     Pagination.resultQuery = resultQuery;
-//     setTimeout(() => {
-//       Pagination.Create(document.getElementById('pagination'));
-//       Pagination.Start();
-//     }, 500);
-//   },
-// };
+  Init: function (size, context, resultQuery) {
+    this.context = context;
+    this.size = size;
+    Pagination.page = 1;
+    this.resultQuery = resultQuery;
+
+    this.Create(document.getElementById('pagination'));
+    this.Start();
+  },
+};
